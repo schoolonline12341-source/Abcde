c191e39200531676f9fb52d4b3f3029de1f6d5c3
ace <schoolonline12341@gmail.com>
2026-06-10 16:35:35 +0200

    Update P1Tab2Setting.lua

--- P1Tab2Setting.lua
+++ P1Tab2Setting.lua
@@ -4,20 +4,15 @@ local MainFrame = _G.MainFrame
 local SettingsPage = _G.SettingsPage
 local UIS = game:GetService("UserInputService")
 local Lighting = game:GetService("Lighting")
-
--- MAKE UIList GLOBAL
 _G.SettingsUIList = Instance.new("UIListLayout", SettingsPage)
 _G.SettingsUIList.HorizontalAlignment = Enum.HorizontalAlignment.Center
 _G.SettingsUIList.Padding = UDim.new(0, 8)
 _G.SettingsUIList.SortOrder = Enum.SortOrder.LayoutOrder
-
 local order = 0
 local function NextOrder()
     order = order + 1
     return order
 end
-
--- GLOBAL
 function CreateTitle(text)
     local t = Instance.new("TextLabel", SettingsPage)
     t.LayoutOrder = NextOrder()
@@ -29,8 +24,6 @@ function CreateTitle(text)
     t.TextSize = 10
     return t
 end
-
--- GLOBAL
 function CreateSetBtn(name)
     local b = Instance.new("TextButton", SettingsPage)
     b.LayoutOrder = NextOrder()
@@ -43,9 +36,7 @@ function CreateSetBtn(name)
     Instance.new("UICorner", b).CornerRadius = UDim.new(0, 6)
     return b
 end
-
 CreateTitle("VISUAL & RENDERING")
-
 local FullBrightBtn = CreateSetBtn("FULL BRIGHT: OFF")
 FullBrightBtn.MouseButton1Click:Connect(function()
     A.FullBright = not A.FullBright
@@ -54,7 +45,6 @@ FullBrightBtn.MouseButton1Click:Connect(function()
     FullBrightBtn.Text = A.FullBright and "FULL BRIGHT: ON" or "FULL BRIGHT: OFF"
     FullBrightBtn.BackgroundColor3 = A.FullBright and Color3.fromRGB(0, 150, 70) or Color3.fromRGB(30, 30, 30)
 end)
-
 local NoFogBtn = CreateSetBtn("NO FOG: OFF")
 NoFogBtn.MouseButton1Click:Connect(function()
     A.NoFog = not A.NoFog
@@ -62,21 +52,16 @@ NoFogBtn.MouseButton1Click:Connect(function()
     NoFogBtn.Text = A.NoFog and "NO FOG: ON" or "NO FOG: OFF"
     NoFogBtn.BackgroundColor3 = A.NoFog and Color3.fromRGB(0, 150, 70) or Color3.fromRGB(30, 30, 30)
 end)
-
 CreateTitle("INTERFACE & STEALTH")
-
 local NamesBtn = CreateSetBtn("HIDDEN NAMES: OFF")
 NamesBtn.MouseButton1Click:Connect(function()
     A.HideNames = not A.HideNames
     for _, v in pairs(game:GetService("Players"):GetPlayers()) do
         if v.Character and v.Character:FindFirstChild("Humanoid") then
-            v.Character.Humanoid.DisplayDistanceType =
-                A.HideNames and Enum.HumanoidDisplayDistanceType.None
-                or Enum.HumanoidDisplayDistanceType.Viewer
+            v.Character.Humanoid.DisplayDistanceType = A.HideNames and Enum.HumanoidDisplayDistanceType.None or Enum.HumanoidDisplayDistanceType.Viewer
         end
     end
     NamesBtn.Text = A.HideNames and "HIDDEN NAMES: ON" or "HIDDEN NAMES: OFF"
     NamesBtn.BackgroundColor3 = A.HideNames and Color3.fromRGB(0, 150, 70) or Color3.fromRGB(30, 30, 30)
 end)
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/schoolonline12341-source/Abcde/main/P2Tab2Setting.lua"))()
